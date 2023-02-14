f5b5f71adb3658e1d67b0470481d03dd0269bee8
Mikhail German <mikhail.german@icloud.com>
2023-02-15 01:40:14 +0500

    add segues

--- LoginApp/LoginViewController.swift
+++ LoginApp/LoginViewController.swift
@@ -9,11 +9,65 @@ import UIKit
 
 final class LoginViewController: UIViewController {
 
-    override func viewDidLoad() {
-        super.viewDidLoad()
+    @IBOutlet var userNameTextField: UITextField!
+    @IBOutlet var passwordTextField: UITextField!
+    @IBOutlet var loginButton: UIButton!
+    @IBOutlet var forgotUserNameButton: UIButton!
+    @IBOutlet var forgotPasswordButton: UIButton!
+    let userName = "1"
+    let password = "2"
+    
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        guard let welcomeVC = segue.destination as? WelcomeViewController else { return }
+        welcomeVC.welcomeText = "Welcome, \(userNameTextField.text ?? "")!"
+        
     }
     
 
-  
+    @IBAction func logInButtonTapped() {
+        if userNameTextField.text != userName || passwordTextField.text != password {
+            showPromptAlert(
+                withTitle: "Warning",
+                andMessage: "Invalid User Name or password!")
+        }
+        
+    }
+    
+    @IBAction func forgotLogInButtonTapped() {
+        showPromptAlert(withTitle: "User Name", andMessage: "Your User Name is \(userName)")
+    }
+    
+    @IBAction func forgotPasswordButtonTapped() {
+        showPromptAlert(withTitle: "Password", andMessage: "Your password is \(password)")
+    }
+    
+    
+    @IBAction func LogOutButtonTapped(_ unwingSegue: UIStoryboardSegue) {
+        guard unwingSegue.source is WelcomeViewController else { return }
+        userNameTextField.text = ""
+        passwordTextField.text = ""
+        
+    }
 
 }
+
+
+
+extension LoginViewController {
+    private func showPromptAlert(withTitle title: String, andMessage message: String) {
+        let alert = UIAlertController(title: title, message: message, preferredStyle: .alert )
+        let okAction = UIAlertAction(title: "Close", style: .default) {_ in
+            self.passwordTextField.text = ""
+        }
+        alert.addAction(okAction)
+        present(alert, animated: true)
+    }
+//    private func showWarningAlert(withTitle: String, andMessage message: String) {
+//        let alert = UIAlertController(title: title, message: message, preferredStyle: .alert )
+//        let okAction = UIAlertAction(title: "OK", style: .default)
+//        alert.addAction(okAction)
+//        present(alert, animated: true)
+//    }
+    
+}
+
--- LoginApp/WelcomeViewController.swift
+++ LoginApp/WelcomeViewController.swift
@@ -8,11 +8,17 @@
 import UIKit
 
 final class WelcomeViewController: UIViewController {
-
+    
+    @IBOutlet var welcomeLabel: UILabel!
+    @IBOutlet var logOutButton: UIButton!
+    
+    var welcomeText: String!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-
+        welcomeLabel.text = welcomeText
+        
     }
-
-
+    
+    
 }
