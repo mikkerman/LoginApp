fc495542d8e49be71b328e46c6ec9156db75c4bd
Mikhail German <mikhail.german@icloud.com>
2023-02-14 13:15:04 +0500

    add stackView

---
+++ LoginApp/LoginViewController.swift
@@ -0,0 +1,19 @@
+//
+//  LoginViewController.swift
+//  LoginApp
+//
+//  Created by Михаил Герман on 14.02.2023.
+//
+
+import UIKit
+
+final class LoginViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+    
+
+  
+
+}
---
+++ LoginApp/WelcomeViewController.swift
@@ -0,0 +1,18 @@
+//
+//  WelcomeViewController.swift
+//  LoginApp
+//
+//  Created by Михаил Герман on 14.02.2023.
+//
+
+import UIKit
+
+final class WelcomeViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+    }
+
+
+}
